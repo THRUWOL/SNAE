179ecfa4697ff343919dcf674fe598015ba21873
Nikita Yarosh <ThRuWol@yandex.ru>
2021-05-10 16:42:37 +0300

    :(

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -52,32 +52,6 @@ namespace SNAE
             double h;
             int iter = 0;
 
-
-
-            if (left != null && right != null)
-            {
-                /* Метод биссекции */
-                if (CBBisection.IsChecked == true)
-                {
-                    TBAnswer.Text = Convert.ToString(Solver.BisectionFindRoot(f, left, right,eps));
-                    
-                }
-                /* Метод секущих */
-                else if (CBSecant.IsChecked == true)
-                {
-                    TBAnswer.Text = Convert.ToString(Solver.SecantFindRoot(f, x_prev, x_curr, eps2));
-                }
-                else if (CBNewton.IsChecked == true)
-                {
-               //     TBAnswer_newton.Text = Convert.ToString(Solver.NewtonRaphson(n1,n2,1,eps3));
-                }
-                else
-                {
-                    // если ничего не выбрано
-                    // sin(y) + 2x = 2
-                    // cos(x-1) + y = 0.7
-                }
-            }
         }
 
         private void CBBisection_Checked(object sender, RoutedEventArgs e)
@@ -119,93 +93,6 @@ namespace SNAE
             Diapazon3.Visibility = Visibility.Hidden;
             GRID1_Copy.Visibility = Visibility.Hidden;
             GRID1.Visibility = Visibility;
-        }
-    }
-
-    public static class Solver
-    {
-        public static double? BisectionFindRoot(Func<double, double> f, double lowerBound, double upperBound,
-            double rangeTolerance = 1e-10, double fTolerance = 1e-3, int maxIterations = 100)
-        {
-            double root = 0.5 * (upperBound + lowerBound);
-            bool convergence = false;
-            double fLowerBound = f(lowerBound);
-            for (int iteration = 0; iteration < maxIterations && !convergence; iteration++)
-            {
-                double fRoot = f(root);
-                double range = Math.Abs(upperBound - lowerBound);
-                convergence = range <= rangeTolerance && Math.Abs(fRoot) <= fTolerance;
-                if (Math.Sign(fRoot) == Math.Sign(fLowerBound))
-                {
-                    lowerBound = root;
-                    fLowerBound = fRoot;
-                }
-                else
-                {
-                    upperBound = root;
-                }
-                root = 0.5 * (upperBound + lowerBound);
-
-            }
-
-            return convergence ? root : (double?)null;
-        }
-
-        public static double? SecantFindRoot(Func<double, double> f, double lowerBound, double upperBound,
-            double rangeTolerance = 1e-10, double fTolerance = 1e-3, int maxIterations = 100)
-        {
-            double fLowerBound = f(lowerBound);
-            double fUpperBound = f(upperBound);
-            double root = upperBound - (upperBound - lowerBound) / (fUpperBound - fLowerBound) * fUpperBound;
-            bool convergence = false;
-            for (int iteration = 0; iteration < maxIterations && !convergence; iteration++)
-            {
-                double fRoot = f(root);
-                double range = Math.Min(upperBound - root, root - lowerBound);
-                convergence = range <= rangeTolerance && Math.Abs(fRoot) <= fTolerance;
-                int fRootSign = Math.Sign(fRoot);
-                if (fRootSign == Math.Sign(fLowerBound))
-                {
-                    lowerBound = root;
-                    fLowerBound = fRoot;
-                }
-                else if (fRootSign == Math.Sign(fUpperBound))
-                {
-                    upperBound = root;
-                    fUpperBound = fRoot;
-                }
-                else
-                {
-                    if (Math.Abs(fLowerBound) > Math.Abs(fUpperBound))
-                    {
-                        fLowerBound = 0;
-                        lowerBound = root;
-                    }
-                    else
-                    {
-                        fUpperBound = 0;
-                        upperBound = root;
-                    }
-                }
-                root = upperBound - (upperBound - lowerBound) / (fUpperBound - fLowerBound) * fUpperBound;
-            }
-
-            return convergence ? root : (double?)null;
-        }
-
-        public static double? NewtonRaphson(Func<double, double> f, Func<double, double> df, double x0,
-            double xTolerance = 1e-10, double fTolerance = 1e-10, int maxIterations = 100)
-        {
-            bool convergence = false;
-            double root = x0;
-            for (int iteration = 0; iteration < maxIterations && !convergence; iteration++)
-            {
-                double fRoot = f(root);
-                double nextRoot = root - fRoot / df(root);
-                convergence = Math.Abs(nextRoot - root) <= xTolerance && Math.Abs(fRoot) <= fTolerance;
-                root = nextRoot;
-            }
-            return convergence ? root : (double?)null;
         }
     }
 }
